5b826492a7901d49f33f7cc98e5e6b21e8386824
khsushan <khsushan@gmail.com>
2015-11-03 08:21:17 +0530

    add quiz model

---
+++ application/models/Quiz_Model.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Quiz_Model
+ *
+ * @author Ushan
+ */
+class Quiz_Model extends CI_Model {
+    
+    //put your code here
+}
