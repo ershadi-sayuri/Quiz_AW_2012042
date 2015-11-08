a4ccb7f8b49f3180d047f8138c0822eda73be685
khsushan <khsushan@gmail.com>
2015-11-08 07:32:08 +0530

    addding quiz model with completed getQuestions method

--- application/models/Quiz_Model.php
+++ application/models/Quiz_Model.php
@@ -9,9 +9,29 @@
 /**
  * Description of Quiz_Model
  *
- * @author Ushan
+ * @author 
  */
 class Quiz_Model extends CI_Model {
-    
-    //put your code here
+
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function getQuestions($category) {
+        $query = $this->db->query("SELECT * FROM question WHERE category_id="
+                . "(SELECT categoryid FROM category WHERE category_name='$category')");
+        foreach ($query->result_array() as $row) {
+            echo $row['questionname']."<br>";
+            echo $row['answer1']."<br>";
+            echo $row['answer2']."<br>";
+            echo $row['answer3']."<br>";
+            echo $row['answer4']."<br>";
+            echo $row['correctanswer']."<br>";
+        }
+    }
+
+    public function calculateScore() {
+        
+    }
+
 }
