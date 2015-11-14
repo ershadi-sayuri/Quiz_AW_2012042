27df4e30cc8bb50e2b4405d40add6d46e26d3411
khsushan <khsushan@gmail.com>
2015-11-14 15:49:33 +0530

    adding getQuestion method

--- application/models/Quiz_Model.php
+++ application/models/Quiz_Model.php
@@ -9,27 +9,31 @@
 /**
  * Description of Quiz_Model
  *
- * @author 
+ * @author
  */
-class Quiz_Model extends CI_Model {
+class Quiz_Model extends CI_Model
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->load->database();
     }
 
-    public function getQuestions($category) {
+    public function getQuestions($category)
+    {
         $query = $this->db->query("SELECT question_id,correctanswer FROM question WHERE category_id= "
-        ."(SELECT categoryid FROM category WHERE category_name='$category')" 
-        ."ORDER BY RAND() LIMIT 10");
-        $array =  array_values($query->result_array());
+            . "(SELECT categoryid FROM category WHERE category_name='$category')"
+            . "ORDER BY RAND() LIMIT 10");
+        $array = array_values($query->result_array());
         echo $array[0]["question_id"];
         return $array;
     }
-    
-    public function getQuestion($question_id){
+
+    public function getQuestion($question_id)
+    {
         $query = $this->db->query("SELECT questionname "
-                . " FROM question WHERE question_id='$question_id'");
+            . " FROM question WHERE question_id='$question_id'");
         return $query->result_array();
     }
-   
+
 }
